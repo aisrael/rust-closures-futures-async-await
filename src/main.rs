8a0efcd8aec945f436092eb87441c3e3675641cd
Alistair A. Israel <aisrael@gmail.com>
2020-05-10 16:00:35 +0800

    Nested blocks and capturing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 fn receives_closure<F>(closure: F)
 where
-    F: Fn(i32, i32) -> i32,
+    F: Fn(i32) -> i32,
 {
-    let result = closure(1, 2);
-    println!("closure(1, 2) => {}", result);
+    let result = closure(1);
+    println!("closure(1) => {}", result);
 }
 
 fn main() {
-    let add = |x, y| x + y;
-    receives_closure(add);
+    {
+        let y = 2;
+        let add = |x| x + y;
+        receives_closure(add);
+    }
+    {
+        let y = 10;
+        let add = |x| x + y;
+        receives_closure(add);
+    }
 }
