f95b8744dc75f8d239eec31e98238ab907565ed6
Alistair A. Israel <aisrael@gmail.com>
2020-05-10 15:47:08 +0800

    receives_closure

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
+fn receives_closure<F>(closure: F)
+where
+    F: Fn(i32, i32) -> i32,
+{
+    let result = closure(1, 2);
+    println!("closure(1, 2) => {}", result);
+}
+
 fn main() {
-    let hello = |who| format!("Hello, {}!", &who);
-    let message = hello("world");
-    println!("{}", message);
+    let add = |x, y| x + y;
+    receives_closure(add);
 }
